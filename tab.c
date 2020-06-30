cd6472119a05e2accc5fe5d4c33bad47a8ced66d
drunknosferatu <tomefelipe0@gmail.com>
2020-06-30 17:10:52 -0300

    <tabuleiro inicializado com uma barra de separação>

--- tab.c
+++ tab.c
@@ -6,30 +6,31 @@ void imprimeTab(peca **tabuleiro,int linha, int coluna){
 	int aux;
 	printf(" ");
 	for (aux=0;aux<coluna;aux++){
-		printf("  %d ",aux);
+		printf(" %d ",aux);
 	}
 	printf("\n");
 	for (int i=0;i<linha;i++){
-		printf(" ");
-		for(aux=0;aux<coluna*4;aux++){
+		printf(" -");
+		for(aux=0;aux<coluna*3;aux++){
 			printf("-");
 		}
 		printf("\n");
-		printf("%d", i);
+		printf("%d", i);			
+		printf("|");
 		for (int j=0;j<coluna;j++){
-			printf("|%c", tabuleiro[i][j].letra);
+			printf("%c", tabuleiro[i][j].letra);
 			printf("%c|",tabuleiro[i][j].num);
 		}
 		printf("%d\n",i);
 	}
-	printf(" ");
-	for(aux=0;aux<coluna*4;aux++){
+	printf(" -");
+	for(aux=0;aux<coluna*3;aux++){
 		printf("-");
 	}
 	printf("\n");
 	printf(" ");
 	for (aux=0;aux<coluna;aux++){
-		printf("  %d ",aux);
+		printf(" %d ",aux);
 	}
 	printf("\n");
 }
